a2d1bfc5326b51183b1ec2c020b69ae54bb96e8c
Vikash Kumar Singh <34976617+vksinghjaipur@users.noreply.github.com>
2024-07-21 21:55:51 +0530

    Add files via upload

---
+++ models/Posts.php
@@ -0,0 +1,24 @@
+<?php
+	
+	namespace app\models;
+	use yii\db\ActiveRecord;
+
+	class Posts extends ActiveRecord
+	{
+		private $title;
+		private $description;
+		private $category;
+
+
+		public function rules(){
+			return[
+
+				[['title', 'description', 'category'], 'required']
+
+			];
+		}
+
+	}
+	
+?>
+
